f5d14e07cf22b2c7dea80e98b652ec5f975d78b9
Sébastien Celles <s.celles@gmail.com>
2018-10-29 11:01:21 +0100

    Remove some deploydocs params

--- docs/make.jl
+++ docs/make.jl
@@ -11,10 +11,5 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/scls19fr/BulkSMS.jl.git",
-    julia  = "1.0",
-    latest = "master",
-    target = "build",
-    deps = nothing,  # we use the `format = :html`, without `mkdocs`
-    make = nothing,  # we use the `format = :html`, without `mkdocs`
+    repo = "github.com/scls19fr/BulkSMS.jl.git"
 )
